86d2817bd8ec714a1eb762d6d7d4737cde34ecca
Ben Musoke-Lubega <bprinsta@gmail.com>
2020-06-24 13:50:40 -0400

    added attributions

--- Quote of the Day/Views/SettingsView.swift
+++ Quote of the Day/Views/SettingsView.swift
@@ -12,6 +12,8 @@ class SettingsView: UIView {
 
     let cardView = CardView()
 	
+	let scrollView = UIScrollView()
+	
 	let logoImage: UIImageView = {
 		let imageView = UIImageView(image: #imageLiteral(resourceName: "in_app_icon"))
 		imageView.contentMode = .scaleAspectFit
@@ -57,6 +59,26 @@ class SettingsView: UIView {
 		return datePicker
 	} ()
 	
+	let theysaidSoAttribution: UILabel = {
+		let label = UILabel()
+		label.font = UIFont(name: "Avenir-Book", size: 12)
+		label.textColor = .systemGray
+		label.textAlignment = .center
+		label.text = "Powered by quotes from theysaidso.com"
+		
+		return label
+	}()
+	
+	let iconsAttribution: UILabel = {
+		let label = UILabel()
+		label.font = UIFont(name: "Avenir-Book", size: 12)
+		label.textColor = .systemGray
+		label.textAlignment = .center
+		label.text = "Icons provided by icons8.com"
+		
+		return label
+	}()
+	
 	// MARK: Methods
 	override init(frame: CGRect) {
 		super.init(frame: frame)
@@ -85,17 +107,23 @@ class SettingsView: UIView {
 
 	func addSubviews() {
 		self.addSubview(cardView)
-		cardView.addSubview(titleLabel)
-		cardView.addSubview(logoImage)
-		cardView.addSubview(versionInfoLabel)
-		cardView.addSubview(pickerLabel)
-		cardView.addSubview(notificationTimePicker)
+		cardView.addSubview(scrollView)
+		scrollView.addSubview(titleLabel)
+		scrollView.addSubview(logoImage)
+		scrollView.addSubview(versionInfoLabel)
+		scrollView.addSubview(pickerLabel)
+		scrollView.addSubview(notificationTimePicker)
+		scrollView.addSubview(theysaidSoAttribution)
+		scrollView.addSubview(iconsAttribution)
 	}
 	
 	func anchorSubviews() {
+		
 		cardView.anchor(top: self.safeAreaLayoutGuide.topAnchor, leading: self.safeAreaLayoutGuide.leadingAnchor, bottom: self.safeAreaLayoutGuide.bottomAnchor, trailing: self.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 12, bottom: 16, right: 12))
 		
-		logoImage.anchor(top: cardView.topAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(top: 48, left: 0, bottom: 0, right:0), size: .init(width: 60, height: 60))
+		scrollView.anchor(top: cardView.topAnchor, leading: cardView.leadingAnchor, bottom: cardView.bottomAnchor, trailing: cardView.trailingAnchor)
+		
+		logoImage.anchor(top: scrollView.topAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(top: 48, left: 0, bottom: 0, right:0), size: .init(width: 60, height: 60))
 		logoImage.centerXAnchor.constraint(equalTo: cardView.centerXAnchor).isActive = true
 		
 		titleLabel.anchor(top: logoImage.bottomAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(top: 16, left: 0, bottom: 0, right: 0))
@@ -107,6 +135,12 @@ class SettingsView: UIView {
 		pickerLabel.anchor(top: versionInfoLabel.bottomAnchor, leading: cardView.leadingAnchor, bottom: nil, trailing: cardView.trailingAnchor, padding: .init(top: 40, left: 16, bottom: 0, right: 16))
 		
 		notificationTimePicker.anchor(top: pickerLabel.bottomAnchor, leading: cardView.leadingAnchor, bottom: nil, trailing: cardView.trailingAnchor, padding: .init(top: 8, left: 16, bottom: 16, right: 16))
+		
+		theysaidSoAttribution.anchor(top: notificationTimePicker.bottomAnchor, leading: cardView.leadingAnchor, bottom: nil, trailing: cardView.trailingAnchor, padding: .init(top: 48, left: 16, bottom: 16, right: 16))
+		
+		iconsAttribution.anchor(top: theysaidSoAttribution.bottomAnchor, leading: cardView.leadingAnchor, bottom: nil, trailing: cardView.trailingAnchor, padding: .init(top: 8, left: 16, bottom: 16, right: 16))
+		
+		
 	}
 
 }
